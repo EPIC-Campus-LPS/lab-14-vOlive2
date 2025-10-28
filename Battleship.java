4b7e804ce2290abfaa6357ffa1d44f9c43c11422
vOlive2.0 <OLiveA148@lpsk12.org>
2025-10-28 09:37:11 -0600

    a

---
+++ Battleship.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Battleship {
+	public static void main(String[] args) {
+		System.out.print("Input: ");
+		Scanner scan = new Scanner(System.in);
+		File f = new File(s.nextLine());
+		scan = new Scanner(f);
+		for(int i = 0; )
+		
+	}
+}
